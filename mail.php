e36ed33bc991319e6338564cd17e5c53a72dcaf1
Jesse Conaway <jesse@jesseconaway.com>
2019-09-18 16:54:24 -0500

    updates

--- mail.php
+++ mail.php
@@ -1,49 +1,66 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$subject = $_POST['subject'];
-header('Content-Type: application/json');
-if ($name === ''){
-print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
-exit();
-}
-if ($email === ''){
-print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
-exit();
-} else {
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
-exit();
-}
-}
-if ($subject === ''){
-print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
-exit();
-}
-if ($message === ''){
-print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
-exit();
-}
 
-if (isset($_POST['g-recaptcha-response'])) {
-		
-    require('component/recaptcha/src/autoload.php');		
-    
-    $recaptcha = new \ReCaptcha\ReCaptcha(6LerJbkUAAAAAFYpl-_j8-i-KudW_A_dheryEzdF, new \ReCaptcha\RequestMethod\SocketPost());
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+        // access
+        $secretKey = '6LerJbkUAAAAAFYpl-_j8-i-KudW_A_dheryEzdF';
+        $captcha = $_POST['g-recaptcha-response'];
 
-      if (!$resp->isSuccess()) {
-            $output = json_encode(array('type'=>'error', 'text' => '<b>Captcha</b> Validation Required!'));
-            die($output);				
-      }	
-}
+        if(!$captcha){
+          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
+          exit;
+        }
 
-$content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "youremail@here.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
-exit();
+        # FIX: Replace this email with recipient email
+        $mail_to = "jesse@jesseconaway.com";
+        
+        # Sender Data
+        $subject = trim($_POST["subject"]);
+        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $phone = trim($_POST["phone"]);
+        $message = trim($_POST["message"]);
+        
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
+            # Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo '<p class="alert alert-warning">Please complete the form and try again.</p>';
+            exit;
+        }
+
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+        $responseKeys = json_decode($response,true);
+
+        if(intval($responseKeys["success"]) !== 1) {
+          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
+        } else {
+            # Mail Content
+            $content = "Name: $name\n";
+            $content .= "Email: $email\n\n";
+            $content .= "Phone: $phone\n";
+            $content .= "Message:\n$message\n";
+
+            # email headers.
+            $headers = "From: $name <$email>";
+
+            # Send the email.
+            $success = mail($mail_to, $subject, $content, $headers);
+            if ($success) {
+                # Set a 200 (okay) response code.
+                http_response_code(200);
+                echo '<p class="alert alert-success">Thank You! Your message has been sent.</p>';
+            } else {
+                # Set a 500 (internal server error) response code.
+                http_response_code(500);
+                echo '<p class="alert alert-warning">Oops! Something went wrong, we couldnt send your message.</p>';
+            }
+        }
+
+    } else {
+        # Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo '<p class="alert alert-warning">There was a problem with your submission, please try again.</p>';
+    }
+
 ?>
