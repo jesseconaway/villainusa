a610c79193e5d4dae348eb1ff8999638d9348e06
Jesse Conaway <jesse@jesseconaway.com>
2019-09-18 16:30:09 -0500

    updates

--- mail.php
+++ mail.php
@@ -25,10 +25,21 @@ if ($message === ''){
 print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
 exit();
 }
-if (strlen($security) != '3'){
-print json_encode(array('security' => 'Incorrect security answer', 'code' => 0));
-exit();
+
+if (isset($_POST['g-recaptcha-response'])) {
+		
+    require('component/recaptcha/src/autoload.php');		
+    
+    $recaptcha = new \ReCaptcha\ReCaptcha(6LerJbkUAAAAAFYpl-_j8-i-KudW_A_dheryEzdF, new \ReCaptcha\RequestMethod\SocketPost());
+
+    $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+      if (!$resp->isSuccess()) {
+            $output = json_encode(array('type'=>'error', 'text' => '<b>Captcha</b> Validation Required!'));
+            die($output);				
+      }	
 }
+
 $content="From: $name \nEmail: $email \nMessage: $message";
 $recipient = "youremail@here.com";
 $mailheader = "From: $email \r\n";
