4082ff85a0f14a96c36aed79b13408981a8baceb
Jesse Conaway <jesse@jesseconaway.com>
2019-09-18 17:28:38 -0500

    updates

--- mail.php
+++ mail.php
@@ -12,16 +12,15 @@
         }
 
         # FIX: Replace this email with recipient email
-        $mail_to = "jesse@jesseconaway.com";
+        $mail_to = "villainfitnessusa@gmail.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $phone = trim($_POST["phone"]);
         $message = trim($_POST["message"]);
         
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo '<p class="alert alert-warning">Please complete the form and try again.</p>';
@@ -38,7 +37,6 @@
             # Mail Content
             $content = "Name: $name\n";
             $content .= "Email: $email\n\n";
-            $content .= "Phone: $phone\n";
             $content .= "Message:\n$message\n";
 
             # email headers.
